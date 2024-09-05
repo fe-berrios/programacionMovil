21045b311a6a74e002e593a143f7b9b5d50cc09d
fe-berrios <fe.berrios.ov@gmail.com>
2024-09-05 12:18:05 -0400

    Se añadío la logica de CRUD del usuario

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -10,14 +10,53 @@ export class UsuarioService {
 
   constructor() { }
 
-  // DAO
-  public createUsuario(usuario:any){}
+  // DAO (Data Access Object)
+  // Su propósito principal es abstraer y encapsular el acceso
+  // a los datos, separando la lógica de persistencia de datos de la lógica de negocio.
+  public createUsuario(usuario:any){
+    // se utiliza this para entrar a las propiedades de la clase/objeto.
+    if(this.getUsuario(usuario.rut) == undefined){
+      // Si 'usuario.rut' no existe (undefined)..
+      this.usuarios.push(usuario);
+      // agregalo a la lista (this.usuarios.push)
+      return true;
+    }
+    return false;
+  }
 
-  public updateUsuario(rut:string, nuevoUsuario:any){}
+  public updateUsuario(rut:string, nuevoUsuario:any){
+    // For each, por cada 'usuario' en 'usuarios'
+    for(let usuario of this.usuarios){
+      // Si el usuario tiene el mismo rut entregado
+      if(usuario.rut == rut){
+        // Usuario se convierte en un 'nuevoUsuario'
+        usuario = nuevoUsuario;
+      }
+    }
+    return false;
+  }
 
-  public deleteUsuario(rut:string){}
+  public deleteUsuario(rut:string){
+    // Se crea constante que contiene el indice de donde esta el usuario con el rut ingresado
+    // Se utiliza 'const' para no poder cambiar el valor de la variable
+    const indice = this.usuarios.findIndex(usuario => usuario.rut == rut);
+    // Si el indice es -1 entonces NO existe el usuario
+    // El -1 es propio del método findIndex
+    if (indice == -1){
+      return false;
+    }
+    this.usuarios.splice(indice, 1);
+    return true;
+  }
 
-  public getUsuario(rut:string){}
+  public getUsuario(rut:string){
+    this.usuarios.find(usuario => usuario.rut == rut);
+    // De la lista anda elemento por elemento (usuario) e itera (=>)
+    // hasta que el elemento.atributo (usuario.rut) coincida (==) con la variable del método (rut).
+  }
 
-  public getUsuarios(){}
+  // Especifica que devuelve una lista[] de algo 'any'
+  public getUsuarios():any[]{
+    return this.usuarios;
+  }
 }
